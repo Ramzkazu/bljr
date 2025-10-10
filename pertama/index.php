3cba89336bae04b623a2d04322d9d0a968d71d11
Ramzkazu <iqrammuhammad818@gmail.com>
2025-10-10 09:47:20 +0700

    Create index.php

---
+++ pertama/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar web programing</title>
+</head>
+<body>
+    <?php 
+        //echo "hello world";
+
+        // variabel (menampung data)
+        $nilai1 = 99;
+        $nilai2 = 88;
+        $nama = "ram";
+        $rata2 = ($nilai1 + $nilai2) / 2;
+        
+        echo "Halo $nama, nilai rata-ratanya adalah $rata2";
+    ?>
+</body>
+</html>
