8de7a4faaad2ee6b6b87b9131bfe570d6405aa37
Ramzkazu <iqrammuhammad818@gmail.com>
2025-11-21 08:33:48 +0700

    Update proses_tambah.php

--- biodata/siswa/proses_tambah.php
+++ biodata/siswa/proses_tambah.php
@@ -2,13 +2,13 @@
 
     #Mengkoneksikan PHP ke MySql
 
-    include("koneksi.php");
+    include("../koneksi.php");
     
     #ambil value dari form
     $nama = $_POST["nama"];
     $nisn = $_POST["nisn"];
     $tp_lahir = $_POST["tp_lahir"];
-    $tg_lahir = $_POST["tg_lahir"];
+    $tgl_lahir = $_POST["tgl_lahir"];
     $alamat = $_POST["alamat"];
     $email = $_POST["email"];
     $jk = $_POST["jk"];
@@ -16,14 +16,14 @@
 
     #proses tambah data
     $query = "INSERT INTO biodata (nama,nisn,tp_lahir,tgl_lahir,alamat,email,jk,jur)
-    VALUES ('$nama','$nisn','$tp_lahir','$tg_lahir','$alamat','$email','$jk','$jur')";
+    VALUES ('$nama','$nisn','$tp_lahir','$tgl_lahir','$alamat','$email','$jk','$jur')";
     
     $tambah = mysqli_query($koneksi, $query);
 
 
     #jk berhsil
     if ($tambah) {
-        header("location:index.php");
+        header("location:../index.php");
     } else {
         echo "Data Gagal ";
     }
