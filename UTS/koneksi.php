61e48b361ca7643b188f633f5ce0670bab955a04
Ramzkazu <iqrammuhammad818@gmail.com>
2025-11-21 08:46:36 +0700

    koneksi php

---
+++ UTS/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $host ="localhost";
+    $username = "root";
+    $password = "";
+    $db = "ordertraker";
+
+    $koneksi = mysqli_connect($host, $username, $password, $ordertraker);
+?>
